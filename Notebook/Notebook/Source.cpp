b6199b1b45d2bc2ffbd6ac1789878d315a84a623
SergiusMagnus <52329866+SergiusMagnus@users.noreply.github.com>
2020-05-14 03:35:32 +0300

    Добавлена возможность поиска

--- Notebook/Notebook/Source.cpp
+++ Notebook/Notebook/Source.cpp
@@ -73,45 +73,60 @@ public:
 		currentLine = name + ' ' + surname + ' ' + patronymic + ' ' + phoneNumber + ' ' + emailAddress + ' ' + birthday + ' ' + city;
 		return currentLine;
 	}
+
+	bool compare(Person sample) {
+		bool trigger = true;
+		if (!((sample.name == name) || (name == "-") || (sample.name == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.surname == surname) || (surname == "-") || (sample.surname == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.patronymic == patronymic) || (patronymic == "-") || (sample.patronymic == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.phoneNumber == phoneNumber) || (phoneNumber == "-") || (sample.phoneNumber == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.emailAddress == emailAddress) || (emailAddress == "-") || (sample.emailAddress == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.birthday == birthday) || (birthday == "-") || (sample.birthday == "-"))) {
+			trigger = false;
+		}
+		if (!((sample.city == city) || (city == "-") || (sample.city == "-"))) {
+			trigger = false;
+		}
+		if (trigger) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 };
+
+void add();
+void search();
 
 int main() {
 	bool keepOn = true;
-	int number;
 	string command;
-	string currentLine;
-	ifstream in;
-	ofstream out;
 
 	cout << "Commands:" << endl;
 	cout << "add - write new contacts from file add.txt." << endl << "    Warning! Data should be recorded in the following order: name, surname," << endl << "    patronymic, phone number, e-mail address, date of birth, city of residence." << endl << "    They should be separated by the symbol ' '. Other use of the ' ' symbol is not allowed." << endl << "    The data for each new person starts with a new line." << endl << "    In case certain data are unknown, they should be replaced with a '-' symbol." << endl;
+	cout << "search - to look for any matching records with the sample." << endl << "    The sample is written by all rules into the 'data to search.txt' file." << endl << "    The result is output to the 'search results.txt' file." << endl;
 	cout << "exit - get out of the program." << endl << endl;
 
 	while (keepOn) {
-		cout << "Enter a command:" << endl;
+		cout << "Enter a command: ";
 		cin >> command;
 
 		if (command == "add") {
-			in.open("add.txt");
-			vector<Person> dataList(10);
-			number = 0;
-
-			while (getline(in, currentLine)) {
-				dataList[number].conversion(currentLine);
-				number++;
-			}
-
-			in.close();
-
-			out.open("data.txt", ios::app);
-
-			for (int i = 0; i < number; i++) {
-				currentLine = dataList[i].compound();
-				out << currentLine << endl;
-			}
-
-			out.close();
-			dataList.clear();
+			add();
+		}
+		else if (command == "search") {
+			search();
 		}
 		else if (command == "exit") {
 			keepOn = false;
@@ -122,4 +137,71 @@ int main() {
 	}
 
 	return 0;
+}
+
+void add() {
+	int number;
+	string currentLine;
+	ifstream in;
+	ofstream out;
+
+	in.open("add.txt");
+	vector<Person> dataList(10);
+	number = 0;
+
+	while (getline(in, currentLine)) {
+		dataList[number].conversion(currentLine);
+		number++;
+	}
+
+	in.close();
+
+	out.open("data.txt", ios::app);
+
+	for (int i = 0; i < number; i++) {
+		currentLine = dataList[i].compound();
+		out << currentLine << endl;
+	}
+
+	out.close();
+
+	cout << "Completed!" << endl << endl;
+}
+
+void search() {
+	int number;
+	string currentLine;
+	ifstream in;
+	ofstream out;
+
+	in.open("data to search.txt");
+	vector<Person> dataList(10);
+	number = 0;
+	Person sample;
+
+	getline(in, currentLine);
+	sample.conversion(currentLine);
+
+	in.close();
+
+	in.open("data.txt");
+
+	while (getline(in, currentLine)) {
+		dataList[number].conversion(currentLine);
+		number++;
+	}
+
+	in.close();
+
+	out.open("search results.txt");
+	for (int i = 0; i < number; i++) {
+		if (dataList[i].compare(sample)) {
+			currentLine = dataList[i].compound();
+			out << currentLine << endl;
+		}
+	}
+
+	out.close();
+
+	cout << "Completed!" << endl << endl;
 }
